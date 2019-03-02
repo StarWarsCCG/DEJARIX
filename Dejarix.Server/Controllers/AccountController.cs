b8081c22a8481a487dd92f4f80ae386dab2ab506
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2019-03-01 21:42:58 -0700

    Fix #2. Fix #4.

--- Dejarix.Server/Controllers/AccountController.cs
+++ Dejarix.Server/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Dejarix.Server.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using System.Text;
 
 namespace Dejarix.Server.Controllers
 {
@@ -14,13 +15,16 @@ namespace Dejarix.Server.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<DejarixUser> _userManager;
+        private readonly SignInManager<DejarixUser> _signInManager;
         private readonly Mailgun _mailgun;
 
         public AccountController(
             UserManager<DejarixUser> userManager,
+            SignInManager<DejarixUser> signInManager,
             Mailgun mailgun)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
             _mailgun = mailgun;
         }
 
@@ -45,7 +49,9 @@ namespace Dejarix.Server.Controllers
             if (result.Succeeded)
             {
                 var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                return Ok(token);
+                var bytes = Encoding.UTF8.GetBytes(token);
+                var hexToken = bytes.ToHex();
+                return Ok(hexToken);
             }
             else
             {
@@ -63,10 +69,67 @@ namespace Dejarix.Server.Controllers
             return View("Register");
         }
 
-        [HttpGet("Verify/{token}")]
-        public IActionResult Verify(string token)
+        [HttpPost("SignIn")]
+        [Consumes("application/x-www-form-urlencoded")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SignInPost()
         {
-            return Ok(token);
+            var request = HttpContext.Request;
+            var formData = await request.ReadFormAsync();
+            var signInUser = formData["sign-in-user"];
+            var signInPass = formData["sign-in-pass"];
+            var user = await _userManager.FindByNameAsync(signInUser);
+            var passwordResult = await _signInManager.CheckPasswordSignInAsync(
+                user, signInPass, false);
+            
+            if (passwordResult.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, true);
+                return Redirect("/");
+            }
+            else
+            {
+                ViewData["SignInError"] = "Failed to sign in.";
+                return View("SignIn");
+            }
+        }
+
+        [HttpGet("SignIn")]
+        public IActionResult SignInGet()
+        {
+            return View("SignIn");
+        }
+
+        [HttpPost("SignOut")]
+        [Consumes("application/x-www-form-urlencoded")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SignOut()
+        {
+            await _signInManager.SignOutAsync();
+            return Redirect("/");
+        }
+
+        [HttpGet("Confirm")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public async Task<IActionResult> Confirm(Guid userId, string token)
+        {
+            var bytes = token.AsHex();
+            var trueToken = Encoding.UTF8.GetString(bytes);
+
+            var user = await _userManager.FindByIdAsync(userId.ToString());
+
+            if (user == null)
+                return Redirect("/");
+
+            var result = await _userManager.ConfirmEmailAsync(user, trueToken);
+
+            if (result.Succeeded)
+            {
+                ViewData["ConfirmedUser"] = user;
+                return View();
+            }
+
+            return Redirect("/");
         }
     }
 }
--- Dejarix.Server/Extensions.cs
+++ Dejarix.Server/Extensions.cs
@@ -1,7 +1,53 @@
+using System;
+
 namespace Dejarix.Server
 {
     static class Extensions
     {
-        
+        private const string HexDigits = "0123456789abcdef";
+        public static string ToHex(this byte[] bytes)
+        {
+            var result = new char[bytes.Length * 2];
+
+            int index = 0;
+            foreach (var b in bytes)
+            {
+                int firstDigit = (b >> 4) & 0x0f;
+                int secondDigit = b & 0x0f;
+
+                result[index++] = HexDigits[firstDigit];
+                result[index++] = HexDigits[secondDigit];
+            }
+
+            return new string(result);
+        }
+
+        private static int FromHex(char c)
+        {
+            if ('0' <= c && c <= '9')
+                return c - '0';
+            else if ('a' <= c && c <= 'f')
+                return c - 'a' + 10;
+            else if ('A' <= c && c <= 'F')
+                return c - 'A' + 10;
+            else
+                throw new ArgumentException("Invalid hex digit: " + c);
+        }
+
+        public static byte[] AsHex(this string text)
+        {
+            var result = new byte[text.Length / 2];
+
+            for (int i = 0; i < result.Length; ++i)
+            {
+                int textIndex = i * 2;
+                int a = FromHex(text[textIndex]);
+                int b = FromHex(text[textIndex + 1]);
+                int digit = (a << 4) | b;
+                result[i] = (byte)digit;
+            }
+
+            return result;
+        }
     }
 }
