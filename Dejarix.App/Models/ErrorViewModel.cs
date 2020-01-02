acc4ecb93ddc6a51df1912c8115002a7df02279f
TheBuzzSaw <TheBuzzSaw@Gmail.com>
2020-01-02 00:05:59 -0700

    Porting app to 3.1.

---
+++ Dejarix.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dejarix.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
